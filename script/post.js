ded0bb10f6bcb9edff046dc053627942398b156c
Hantama <pratamahantama@gmail.com>
2019-07-22 08:37:16 +0800

    ready to use
    need more improvement

--- script/post.js
+++ script/post.js
@@ -47,7 +47,14 @@ class Post{
     }
     //create an anchor element 
     createLink(text,source){
-        return "<a href=https://"+source+">"+text+"</a>"
+        return "<a class='link' href=https://"+source+">"+text+"</a>"
+    }
+    createImg(source,height,width){
+        return "<img class='postimg' src="+source+" height="+(height?height:'auto')+" width="+(width?width:400)+">"
+        //return "<img src='"+source+"' height='"+height?height:auto+"' width='"+width?width:400+"' >"
+    }
+    createCodeSection(text){
+        return ("<pre><code>"+text+"</code></pre>")
     }
 }
 
@@ -58,14 +65,22 @@ that the first time I try to create it I got some error that maybe you have face
 ReactNative.addParagraph(`
 Maybe you have experienced that when you create react native monorepos using yarn workspaces there is some react native library 
 that is become not found especially in expo when you run yarn start there would be error because expo couldn't find the dependency
-in the node_modules folder. And here is how I create the monorepos :
-`)
-ReactNative.addSubtitle("First set up yarn workspaces normaly as you do")
+in the node_modules folder. The poin to make it work is by using nohoist feature of the yarn workspaces. And here is how I create the monorepos :
+`+ReactNative.createCodeSection(
+`asdasd;asdasd;
+    if(a===3){aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        asd;
+    }
+`
+))
+ReactNative.addSubtitle("1. First set up yarn workspaces normaly as you do")
 ReactNative.addParagraph(`
 Create a directory for `+ReactNative.createLink("myapi","plumiertest123.herokuapp.com")+` yarn workspace and modify package.json add the workspaces
-`)
+`+ReactNative.createImg('assets/img/post1.jpg'))
 
 const Post2= new Post();
+Post2.setTitle("List of best game that I have played")
+Post2.setDescription("This post is discussing about best game that I've played, mostly I played Racing game,RPG, Action and MOBA. I played both online and offline game which is now I play online game at the most")
 
 
 let postList=[ReactNative,Post2]
